5cc147a0eef61a19b02e947bd5b0b23978493b9a
WHITELOTUS <126754757+WHITELOTUS4@users.noreply.github.com>
2024-11-12 17:25:53 +0530

    Version 1.0

--- module/imgConverter.py
+++ module/imgConverter.py
@@ -1,33 +1,50 @@
-# import cv2
 import numpy as np
 import Preprocessor
 import svgwrite
 import base64
-from PIL import Image
+from PIL import Image, ImageOps
 import io
 
-def convert_png(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    if image is None:
-        raise ValueError("Image not loaded properly. Check the file path.")
-    if len(image.shape) == 3 and image.shape[2] == 4:
-        filename, _ = image_path.rsplit('.', 1)
-        new_image_path = f"{filename}.png"
-        cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-    else:
-        if len(image.shape) == 2 or image.shape[2] == 1:
-            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGRA)
-        else:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
-        if image.shape[2] == 3:
-            alpha_channel = np.ones((image.shape[0], image.shape[1]), dtype=np.uint8) * 255
-            image = np.dstack((image, alpha_channel))
-        filename, _ = image_path.rsplit('.', 1)
-        new_image_path = f"{filename}.png"
-        cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-    return new_image_path
+# def convert_png(image_path):
+#     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+#     if image is None:
+#         raise ValueError("Image not loaded properly. Check the file path.")
+#     if len(image.shape) == 3 and image.shape[2] == 4:
+#         filename, _ = image_path.rsplit('.', 1)
+#         new_image_path = f"{filename}.png"
+#         cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+#     else:
+#         if len(image.shape) == 2 or image.shape[2] == 1:
+#             image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGRA)
+#         else:
+#             image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+#         if image.shape[2] == 3:
+#             alpha_channel = np.ones((image.shape[0], image.shape[1]), dtype=np.uint8) * 255
+#             image = np.dstack((image, alpha_channel))
+#         filename, _ = image_path.rsplit('.', 1)
+#         new_image_path = f"{filename}.png"
+#         cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+#     return new_image_path
+
+def convert_png(base64_image_path):
+    if not base64_image_path.startswith("data:image"):
+        return 1
+    _, base64_data = base64_image_path.split(',', 1)
+    image_data = base64.b64decode(base64_data)
+    image = Image.open(io.BytesIO(image_data)).convert("RGBA")
+    if image.mode != "RGBA":
+        image = ImageOps.alpha_composite(
+            Image.new("RGBA", image.size, (255, 255, 255, 255)), image.convert("RGBA")
+        )
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG", compress_level=0)
+    new_base64_image = "data:image/png;base64," + base64.b64encode(buffered.getvalue()).decode()
+    return new_base64_image
+
 
 def convert_jpg(image_data):
+    if not image_data.startswith("data:image"):
+        return 1
     image_bytes = base64.b64decode(image_data.split(",")[1])
     image = Image.open(io.BytesIO(image_bytes))
     image = image.convert("RGB")
@@ -36,60 +53,59 @@ def convert_jpg(image_data):
     jpg_image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
     return f"data:image/jpg;base64,{jpg_image_base64}"
 
-def convert_jpeg(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    if image.shape[2] == 4:
-        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-    elif image.shape[2] == 1:
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    filename, extension = image_path.rsplit('.', 1)
-    new_image_path = f"{filename}.jpeg"
-    cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    return new_image_path
+def convert_jpeg(image_data):
+    if not image_data.startswith("data:image"):
+        return 1
+    image_bytes = base64.b64decode(image_data.split(",")[1])
+    image = Image.open(io.BytesIO(image_bytes))
+    if image.mode not in ["RGB", "L"]:
+        image = image.convert("RGB")
+    elif image.mode == "L":
+        image = image.convert("RGB")
+    buffer = io.BytesIO()
+    image.save(buffer, format="JPEG", quality=100)
+    jpeg_image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return f"data:image/jpeg;base64,{jpeg_image_base64}"
 
 def convert_gif(image_path):
     return image_path
 
-def convert_bmp(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    if image.shape[2] == 4:
-        b, g, r, a = cv2.split(image)
-        b = cv2.bitwise_and(b, b, mask=a)
-        g = cv2.bitwise_and(g, g, mask=a)
-        r = cv2.bitwise_and(r, r, mask=a)
-        image = cv2.merge((b, g, r))
-    elif image.shape[2] == 1:
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    else:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
-        alpha_channel = np.ones((image.shape[0], image.shape[1]), dtype=image.dtype) * 255
-        image = np.dstack((image, alpha_channel))
-    filename, extension = image_path.rsplit('.', 1)
-    new_image_path = f"{filename}.bmp"
-    cv2.imwrite(new_image_path, image)
-    return new_image_path
+def convert_bmp(image_data):
+    if not image_data.startswith("data:image"):
+        return 1
+    image_bytes = base64.b64decode(image_data.split(",")[1])
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.convert("RGBA")
+    r, g, b, a = image.split()
+    b = ImageOps.autocontrast(Image.composite(b, Image.new("L", b.size, 0), a))
+    g = ImageOps.autocontrast(Image.composite(g, Image.new("L", g.size, 0), a))
+    r = ImageOps.autocontrast(Image.composite(r, Image.new("L", r.size, 0), a))
+    image = Image.merge("RGB", (r, g, b))
+    buffer = io.BytesIO()
+    image.save(buffer, format="BMP")
+    jpeg_image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return f"data:image/bmp;base64,{jpeg_image_base64}"
 
-def convert_svg(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    if image.shape[2] == 4:
-        b, g, r, a = cv2.split(image)
-        image = cv2.merge((r, g, b, a))
-    else:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        alpha_channel = np.ones((image.shape[0], image.shape[1]), dtype=image.dtype) * 255
-        image = np.dstack((image, alpha_channel))
-    height, width, _ = image.shape
-    filename, extension = image_path.rsplit('.', 1)
-    new_image_path = f"{filename}.svg"
-    dwg = svgwrite.Drawing(new_image_path, size=(width, height))
+def convert_svg(image_data):
+    if not image_data.startswith("data:image"):
+        return 1
+    image_bytes = base64.b64decode(image_data.split(",")[1])
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGBA")
+    image_np = np.array(image)
+    height, width, _ = image_np.shape
+    dwg = svgwrite.Drawing(size=(width, height))
     for y in range(height):
         for x in range(width):
-            r, g, b, a = image[y, x]
+            r, g, b, a = image_np[y, x]
             if a > 0:
                 fill = svgwrite.rgb(r, g, b, '%')
                 dwg.add(dwg.rect(insert=(x, y), size=(1, 1), fill=fill, fill_opacity=a / 255.0))
-    dwg.save()
-    return new_image_path
+    buffer = io.StringIO()
+    dwg.write(buffer)
+    svg_content = buffer.getvalue()
+    buffer.close()
+    svg_base64 = base64.b64encode(svg_content.encode("utf-8")).decode("utf-8")
+    return f"data:image/svg+xml;base64,{svg_base64}"
 
 def convert_tga(image_path):
     return image_path
@@ -101,6 +117,10 @@ def convert_image(input_list):
     image_path = str(input_list[0])
     extension = str(input_list[1])
     new_image_path = None
+
+    if(image_path=='load'):
+        image_path = Preprocessor.Tools.merge_list_to_string(Preprocessor.single_img_bin)
+        Preprocessor.single_img_bin.clear()
 
     if Preprocessor.Tools.is_image(image_path) == True:
         if extension == 'png':
