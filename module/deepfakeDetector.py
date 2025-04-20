8341019f0737942a67c5018390498cf93618efb5
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2025-04-20 21:13:26 +0530

    Version 1.0

--- module/deepfakeDetector.py
+++ module/deepfakeDetector.py
@@ -1,7 +1,8 @@
 import os
 import Preprocessor
 from module import dfd_p1
-from module import dfd_p2
+# from module import dfd_p2
+from module import dfd_p3
 
 module_dir = os.path.dirname(__file__)
 if os.path.exists(os.path.join(module_dir, 'temp.py')):
@@ -11,13 +12,18 @@ def hard_voting(voters):
     real = 0
     fake = 0
     total_accuracy = 0
-    for i in range(0, len(voters), 1):
+    if len(voters) % 2 == 0:
+        limit = len(voters) - 1
+    else:
+        limit = len(voters)
+    
+    for i in range(0, limit, 1):
         if voters[i]["class"] == 'Real':
             real += 1
         elif voters[i]["class"] == 'Fake':
             fake += 1
         total_accuracy += voters[i]["accuracy"]
-    accuracy = round((total_accuracy / len(voters)), 2)
+    accuracy = round((total_accuracy / limit), 2)
     winer = 'Real' if real > fake else 'Fake'
     return {"class": winer, "accuracy": accuracy}
 
@@ -30,7 +36,9 @@ def detect_image(input_list, no_of_model):
         if(input_list[0]=='load'):
             input_list[0] = Preprocessor.Tools.merge_list_to_string(Preprocessor.single_img_bin.copy())
         p1 = dfd_p1.detect_image(input_list)
-        p2 = dfd_p2.detect_image(input_list)
-        src = Preprocessor.MutableDict(p1).insert("responce_tree", {"prototype_1": p1, "prototype_2": p2})
+        p2 = dfd_p3.detect_image(input_list)
+        p3 = dfd_p3.detect_image(input_list)
+        final_result = hard_voting([p1, p2, p3])
+        src = Preprocessor.MutableDict(final_result).insert("responce_tree", {"prototype_1": p1, "prototype_2": p2, "prototype_3": p3})
         Preprocessor.single_img_bin.clear()
         return src
